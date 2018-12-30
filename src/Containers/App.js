379f2bb4233a02da3e46a1d8a5faf3286fd4c4be
Prasanna Sriram <pras306@gmail.com>
2018-12-31 03:18:40 +0530

    redux implementation for state management

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,27 +1,38 @@
 import React, { Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../Components/CardList.js';
 import SearchBox from '../Components/SearchBox.js';
 import Scroll from '../Components/Scroll.js';
 import './App.css';
+import {setSearchField} from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange : (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component{
 	constructor(){
 		super();
 		this.state = {
-			robots: [],
-			searchField: ''
+			robots: []			
 		}
 	}
-	componentDidMount(){
+	componentDidMount(){		
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(users => this.setState({robots: users}));
-	}
-	onSearchChange = (event) => {
-		this.setState({searchField: event.target.value })
-	}
+	}	
 	render(){
-		const {robots, searchField} = this.state;
+		const {robots} = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter( robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		});		
@@ -29,7 +40,7 @@ class App extends Component{
 			(!robots.length) ? <h1>Loading</h1> :
 			<div className='tc'>
 				<h1>RoboFriends</h1>
-				<SearchBox searchChange = {this.onSearchChange}/>
+				<SearchBox searchChange = {onSearchChange}/>
 				<Scroll>
 					<CardList robots={filteredRobots} />
 				</Scroll>				
@@ -39,4 +50,4 @@ class App extends Component{
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
